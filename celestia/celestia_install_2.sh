1cf212c08c70e80a63cdb45aae1aed754b433179
haku1806 <thangtranhtn2000@gmail.com>
2022-11-07 09:17:00 +0700

    Fix bug celestia

--- celestia/celestia_install_2.sh
+++ celestia/celestia_install_2.sh
@@ -1,28 +1,26 @@
 #!/bin/sh
 
-# wget -O celestia_install_step1.sh https://raw.githubusercontent.com/haku1806/testnet_node/main/celestia/celestia_install_step1.sh && chmod +x celestia_install_step1.sh && ./celestia_install_step1.sh
-# wget -O celestia_install_2.sh https://raw.githubusercontent.com/haku1806/testnet_node/main/celestia/celestia_install_2.sh && chmod +x celestia_install_2.sh && ./celestia_install_2.sh
-# sh <(curl https://raw.githubusercontent.com/haku1806/testnet_node/main/celestia/celestia_install_step1.sh || wget -O - https://raw.githubusercontent.com/haku1806/testnet_node/main/celestia/celestia_install_step1.sh)
-# sh <(curl https://raw.githubusercontent.com/haku1806/testnet_node/main/celestia/celestia_install_2.sh || wget -O - https://raw.githubusercontent.com/haku1806/testnet_node/main/celestia/celestia_install_2.sh)
-
-
-PS3='Please enter your choice: '
-options=("Option 1" "Option 2" "Option 3" "Quit")
+PS3='Select an action: '
+options=(
+"Install Node"
+"Check Log"
+"Exit")
 select opt in "${options[@]}"
 do
 case $opt in
-"Option 1")
-echo "you chose choice 1"
-;;
-"Option 2")
-echo "you chose choice 2"
-;;
-"Option 3")
-echo "you chose choice $REPLY which is $opt"
-;;
-"Quit")
+"Install Node")
+echo "Install node"
 break
+;;
+
+"Check Log")
+echo "Check Log"
+break;
+;;
+"Exit")
+exit
 ;;
 *) echo "invalid option $REPLY";;
 esac
+done
 done
