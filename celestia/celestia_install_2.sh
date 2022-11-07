646ce9365d894c733cc5a828f9dc7d0491f2e919
haku1806 <thangtranhtn2000@gmail.com>
2022-11-07 08:59:20 +0700

    fix celestia

--- celestia/celestia_install_2.sh
+++ celestia/celestia_install_2.sh
@@ -2,31 +2,6 @@
 # wget -O celestia_install_step1.sh https://raw.githubusercontent.com/haku1806/testnet_node/main/celestia/celestia_install_step1.sh && chmod +x celestia_install_step1.sh && ./celestia_install_step1.sh
 # sh <(curl https://raw.githubusercontent.com/haku1806/testnet_node/main/celestia/celestia_install_step1.sh || wget -O - https://raw.githubusercontent.com/haku1806/testnet_node/main/celestia/celestia_install_step1.sh)
 # sh <(curl https://raw.githubusercontent.com/haku1806/testnet_node/main/celestia/celestia_install_2.sh || wget -O - https://raw.githubusercontent.com/haku1806/testnet_node/main/celestia/celestia_install_2.sh)
-OUTPUT=$(cat /etc/*release)
-if echo $OUTPUT | grep -q "Ubuntu 20.04" ; then
-        echo -e "\nDetecting Ubuntu 20.04...\n"
-        SERVER_OS="Ubuntu20"
-else
-    if  echo $OUTPUT | grep -q "CentOS Linux 7" ; then
-            echo -e "\nDetecting Centos 8...\n"
-    elif echo $OUTPUT | grep -q "CentOS Linux 8" ; then
-            echo -e "\nDetecting Centos 8...\n"
-    elif echo $OUTPUT | grep -q "CloudLinux 7" ; then
-            echo -e "\nDetecting CloudLinux 7...\n"
-    elif echo $OUTPUT | grep -q "AlmaLinux 8" ; then
-            echo -e "\nDetecting AlmaLinux 8...\n"
-    elif echo $OUTPUT | grep -q "Rocky Linux" ; then 
-            echo -e "\nDetecting Rocky Linux...\n"
-    elif echo $OUTPUT | grep -q "Ubuntu 18.04" ; then
-            echo -e "\nDetecting Ubuntu 18.04...\n"
-    elif echo $OUTPUT | grep -q "Ubuntu 22.04" ; then
-            echo -e "\nDetecting Ubuntu 22.04...\n"
-    else
-            echo -e "\nUnable to detect your OS...\n"
-    fi
-    echo -e "Celedes is supported on Ubuntu 20.04\n"
-    exit 1
-fi
 
 
 PS3='Please enter your choice: '
@@ -39,28 +14,28 @@ options=(
 )
 select opt in "${options[@]}"
 do
-    case $opt in
+case $opt in
         "Install Node")
                 # Snapshoot version number
                 echo -e "\nSnapshoot version number 2022-11-06."
                 # INIT SETTINGS
                 echo -e "\nInit wallet config\nExample:\n CELESTIA_NODENAME=\"DuhItsAniketNode\"\nCELESTIA_WALLET=\"DuhItsAniketWallet\"\nCELESTIA_CHAIN=\"mamaki\"\n\n"
-                
+
                 # echo '=============== SETUP FINISHED ==================='
                 ;;
 
         "Check Log")
+                echo -e "Check log"
                 journalctl -u celestia-appd -f -o cat
                 ;;
 
         "Check Sync")
+                echo -e "Check sync"
                 curl -s localhost:${CELESTIA_PORT}657/status | jq .result.sync_info
                 ;;
 
         "Request token in Discord)
-                echo "========================================================================================================================"
-                echo "You can request from Mamaki Testnet Faucet on the #faucet channel on Celestia's Discord server with the following command: $request <CELESTIA_ADDR>"
-                echo "========================================================================================================================"
+                echo -e "Request token in Discord"
                 ;;
 
         "Quit")
@@ -68,5 +43,4 @@ do
             ;;
         *) echo "invalid option $REPLY";;
     esac
-done
 done
